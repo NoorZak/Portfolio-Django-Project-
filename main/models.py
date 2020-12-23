a447f4967672b6dad2b24daccb53a5f2be79c70c
NoorZak <n.zakarneh1@student.aaup.edu>
2020-12-23 10:41:07 +0200

    commit13

--- main/models.py
+++ main/models.py
@@ -1,3 +1,17 @@
 from django.db import models
 
 # Create your models here.
+class User(models.Model):
+
+    first_name = models.CharField(max_length=45)
+    last_name = models.CharField(max_length=45)
+    email=models.CharField(max_length=45)
+    password = models.CharField(max_length=255)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+    imgUrl = models.CharField(max_length=255)
+
+
+    #objects = UserManager()
+    def __str__(self):
+        return f"<User object: {self.first_name+self.last_name} ({self.id})>"
